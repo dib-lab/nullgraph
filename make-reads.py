a6abd660e3f08ba244677465bc183dca58a52180
Jordan Fish <jrdn.fish@gmail.com>
2013-09-16 01:11:46 -0400

    Modified make-reads.py to output detailed mutation information (and also not consider changing to the same base a mutation

--- make-reads.py
+++ make-reads.py
@@ -18,6 +18,8 @@ n_reads = int(len_genome*COVERAGE / float(READLEN))
 reads_mut = 0
 total_mut = 0
 
+nucl = ['a', 'c', 'g', 't']
+
 for i in range(n_reads):
     start = random.randint(0, len_genome - READLEN)
     read = genome[start:start + READLEN].upper()
@@ -28,17 +30,27 @@ for i in range(n_reads):
 
     # error?
     was_mut = False
+    seq_name = "read{0}".format(i)
     for _ in range(READLEN):
         while random.randint(1, ERROR_RATE) == 1:
            pos = random.randint(1, READLEN) - 1
-           read = read[:pos] + random.choice(['a', 'c', 'g', 't']) + read[pos+1:]
+
+           new_base = random.choice(nucl)
+           orig = read[pos]
+
+           if orig.lower() == new_base:
+               continue
+
+           print >>sys.stderr, "{0}\t{1}\t{2}\t{3}".format(seq_name, pos, orig, new_base)
+
+           read = read[:pos] + new_base + read[pos+1:]
            was_mut = True
            total_mut += 1
 
     if was_mut:
         reads_mut += 1
     
-    print '>read%d\n%s' % (i, read)
+    print '>{0}\n{1}'.format(seq_name, read)
 
 print >>sys.stderr, "%d of %d reads mutated; %d total mutations" % \
     (reads_mut, n_reads, total_mut)
